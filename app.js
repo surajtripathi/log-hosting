d0fb010e560c1827c2f948715d6623833eb3986e
Suraj Tripathi <suraj99934@gmail.com>
2016-07-27 22:31:48 +0530

    logs reads from file and send on server

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var socket_io = require('socket.io');
 var http = require('http');
 var express = require('express');
+var Transform = require('stream').Transform;
+var util = require('util');
+var split = require("split");
+
+var tailStream = require('tail-stream');
+var tail_stream = tailStream.createReadStream('./example.txt', {interval: 100});
 
 var app = express();
 
@@ -14,17 +20,27 @@ app.get('/_status', function (req, res) {
 var server = http.createServer(app);
 io = socket_io(server);
 
+util.inherits(GetDataStream, Transform);
+
+function GetDataStream() {
+	Transform.call(this);
+}
+
 io.on('connection', function (socket) {
 	console.log("connected");
 	socket.on('disconnect', function () {
 		console.log("disconnected");
 	});
 	socket.on('message sent', function (data) {
-		console.log("message recieved", data.message);
-		io.emit('chat', {message: data.message});
+		io.emit('chat1', {message: data.message});
 	});
-
 });
 
+GetDataStream.prototype._transform = function(line, encoding, processed) {
+	io.emit('chat', {message: line.toString('utf8')});
+	processed();
+}
+
+tail_stream.pipe(split()).pipe(new GetDataStream());
 
 server.listen('3000');
